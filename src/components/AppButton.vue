bf306cfc2645aa44c96f68ffa438d33a5108df0b
Khalifa1997 <ahmed70000@gmail.com>
2022-12-25 21:50:42 +0200

    Added AppButton

---
+++ src/components/AppButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="bg-purple-800 rounded-lg px-4 py-2 text-white">
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    text: String,
+  },
+});
+</script>
+
+<style scoped></style>
