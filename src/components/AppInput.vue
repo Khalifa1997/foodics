4b0e8c2352b7e6e70d367ddfe305fff2aa882ebd
Khalifa1997 <ahmed70000@gmail.com>
2022-12-27 01:35:24 +0200

    added app input

---
+++ src/components/AppInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <input
+    class="border-2 border-gray-300 rounded-md px-2 py-1"
+    style="min-width: 100%"
+    :value="value"
+    @input="updateValue"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    value: String,
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
