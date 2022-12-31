f4760fc56f6b2196e0f24c5ae9b493490673a564
Khalifa1997 <ahmed70000@gmail.com>
2022-12-31 22:46:23 +0200

    add timeslot component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+import PrimeVue from "primevue/config";
+import InputMask from "primevue/inputmask";
+
+Vue.use(PrimeVue);
+
+Vue.component("InputMask", InputMask);
 
 new Vue({
   render: (h) => h(App),
