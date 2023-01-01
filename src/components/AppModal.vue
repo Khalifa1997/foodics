e1f665616c2ce97dedf0c639203191e7524204a3
Khalifa1997 <ahmed70000@gmail.com>
2023-01-01 02:52:22 +0200

    reworked appmodal

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="relative z-10" role="dialog" aria-modal="true">
-    <div
-      class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
-    >
-      <slot></slot>
+  <div
+    :class="[
+      'fixed overflow-auto bg-black bg-opacity-90 left-0 top-0 mx-auto w-full h-full z-1 ',
+    ]"
+    :style="modelValue ? 'display:block;' : 'display:none;'"
+  >
+    <div class="p-2 bg-white opacity-100 w-5/6 mx-auto my-5">
+      <slot name="body"></slot>
+      <div class="bg-gray-50 px-4 py-3 flex gap-3 justify-end">
+        <slot name="footer" :close="toggleShow"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    modelValue: Boolean,
+  },
+  model: {
+    prop: "modelValue",
+    event: "update:modelValue",
+  },
+  methods: {
+    toggleShow() {
+      this.$emit("update:modelValue", false);
+    },
+  },
+});
 </script>
 
 <style scoped></style>
