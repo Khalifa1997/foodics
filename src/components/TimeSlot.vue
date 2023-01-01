235dca478913bfc3a2ce27ff2e88ff99e8cd5f07
Khalifa1997 <ahmed70000@gmail.com>
2023-01-01 02:51:40 +0200

    fixed timeslot component

--- src/components/TimeSlot.vue
+++ src/components/TimeSlot.vue
@@ -1,14 +1,17 @@
 <template>
   <div
-    class="flex gap-2 mx-auto border-2 border-purple-400 p-1 w-32 rounded-lg justify-center"
+    class="flex gap-2 border-2 border-purple-400 p-1 w-32 rounded-lg justify-between relative"
   >
     <InputMask
       v-model="from"
       mask="99-99"
       slotChar="hh:mm"
       :class="{
-        'outline-none w-12 text-xs': true,
-        'bg-red-100 border-2 border-red-600 rounded-lg': fromError,
+        'outline-none w-12 text-xs text-center': true,
+        'bg-red-100 border-2 border-red-600 rounded-lg':
+          fromError || !isBetween,
+        'bg-green-100 border-2 border-green-600 rounded-lg':
+          !fromError && isBetween,
       }"
     />
     <span>-</span>
@@ -17,10 +20,19 @@
       mask="99-99"
       slotChar="hh:mm"
       :class="{
-        'outline-none w-12 text-xs': true,
-        'bg-red-100 border-2 border-red-600 rounded-lg': toError,
+        'outline-none w-12 text-xs text-center': true,
+        'bg-red-100 border-2 border-red-600 rounded-lg': toError || !isBetween,
+        'bg-green-100 border-2 border-green-600 rounded-lg':
+          !fromError && isBetween,
       }"
     />
+    <div
+      class="absolute -top-3 w-4 h-4 bg-red-50 flex justify-center border-2 border-red-500 rounded-full p-1"
+      @click="removeTimeSlot"
+      style="left: 7.3rem"
+    >
+      <img src="../assets/close.png" width="10px" height="10px" />
+    </div>
   </div>
 </template>
 <script>
@@ -35,12 +47,12 @@ export default {
     toError: false,
   }),
   props: {
-    time: Array,
+    time: Object,
   },
-  emits: ["addTime"],
+  emits: ["addTimeSlot", "removeTimeSlot"],
   model: {
     prop: "time",
-    event: "addTime",
+    event: "addTimeSlot",
   },
   methods: {
     containsLetter(string) {
@@ -53,35 +65,65 @@ export default {
     minuteInsideLimit(number) {
       return number < 60 && number >= 0;
     },
+    isRightFormat(string) {
+      return (
+        !this.containsLetter(string) &&
+        this.hourInsideLimit(string.split("-")[0]) &&
+        this.minuteInsideLimit(string.split("-")[1])
+      );
+    },
+    removeTimeSlot() {
+      this.$emit("removeTimeSlot", this.time.id);
+    },
+  },
+  computed: {
+    isBetween: function () {
+      return (
+        this.to.split("-")[0] > this.from.split("-")[0] ||
+        (this.to.split("-")[0] === this.from.split("-")[0] &&
+          this.to.split("-")[1] > this.from.split("-")[1])
+      );
+    },
   },
   watch: {
     from(newValue) {
       this.fromError = false;
-      if (this.containsLetter(newValue)) this.fromError = true;
-      if (!this.hourInsideLimit(newValue.split("-")[0])) this.fromError = true;
-      if (!this.minuteInsideLimit(newValue.split("-")[1]))
-        this.fromError = true;
+      this.fromError = !this.isRightFormat(newValue);
+
       if (
         !this.fromError &&
         !this.toError &&
         this.to.length &&
-        this.from.length
+        this.from.length &&
+        this.isBetween
       )
-        this.$emit("addTime", [this.from, this.to]);
+        this.$emit("addTimeSlot", {
+          ...this.time,
+          from: this.from,
+          to: this.to,
+        });
     },
     to(newValue) {
       this.toError = false;
-      if (this.containsLetter(newValue)) this.toError = true;
-      if (!this.hourInsideLimit(newValue.split("-")[0])) this.toError = true;
-      if (!this.minuteInsideLimit(newValue.split("-")[1])) this.toError = true;
+      this.toError = !this.isRightFormat(newValue);
+
       if (
         !this.fromError &&
         !this.toError &&
         this.to.length &&
-        this.from.length
+        this.from.length &&
+        this.isBetween
       )
-        this.$emit("addTime", [this.from, this.to]);
+        this.$emit("addTimeSlot", {
+          ...this.time,
+          from: this.from,
+          to: this.to,
+        });
     },
+  },
+  created() {
+    this.from = this.time.from || "";
+    this.to = this.time.to || "";
   },
 };
 </script>
